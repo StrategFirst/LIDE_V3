61ac6d4338842bddf6b371b98684328551f557c5
tanguy <tanguy.jouvin@hotmail.com>
2021-11-15 13:53:38 +0100

    remove lide-app-master

--- LIDE-APP-master/lide-back/images/build_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker build  --no-cache --tag  py_lide ./python/
-docker build  --no-cache --tag  cpp_lide ./cpp/
-docker build  --no-cache --tag  java_lide ./java/
-docker build  --no-cache --tag  php_lide ./php/

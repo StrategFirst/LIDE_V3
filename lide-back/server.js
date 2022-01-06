d5c9d1877aa544b740860586aaf28fd6bbeb2188
StrategFirst <mathieu.toulon@free.fr>
2022-01-06 03:19:02 +0100

    Clarification et préparation HTTPS Retrait du code inutile Préparation de l'implémentation pour l'HTTPS Et clarification du code pour l'HTTP

--- lide-back/server.js
+++ lide-back/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = require("./router");
 const cors = require("cors");
+const http = require("http");
 const https = require("https");
 const fs = require("fs");
-const bodyParser = require('body-parser');
 
-const port = 3000;
+const portHTTP = process.env.SERVER_PORT;
 const app = express();
 const db = require("./db");
 const { request } = require("http");
@@ -15,17 +15,20 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 
-/*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});*/
 /* --- Ensemble des routes --- */
 app.use("/api/v1", router);
 
 /* --- Lancement du serveur back --- */
-app.listen(port, () => {
-  console.log("Server starting : http://localhost:" + port);
-});
+let httpServer = http.createServer( app );
+
+httpServer.listen( portHTTP , () => console.log(`HTTP ready on port ${portHTTP}`) );
+
+
+/*
+ Si HTTPS prêt :
+
+const portHTTPS = // à définir //;
+let httpsServer = https.createServer( credentials , app );
+httpsServer.listen( portHTTPS , () => console.log(`HTTPS ready on port ${portHTTPS}`) );
+
+*/

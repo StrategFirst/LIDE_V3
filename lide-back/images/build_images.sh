a964e2d872c2e6ece57ba62460565a14d18aa381
StrategFirst <mathieu.toulon@free.fr>
2022-01-06 03:16:44 +0100

    Factorisation et optimisation Plutôt que de se répéter une boucle avec une liste pour parcourrir toutes les images à build Et aussi plutôt que de faire les images une à une on les lances en parrralelles et on attend qu'elles soit toutes build pour aller plus vites

--- lide-back/images/build_images.sh
+++ lide-back/images/build_images.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-docker build  --no-cache --tag  py_lide ./py/
-docker build  --no-cache --tag  cpp_lide ./cpp/
-docker build  --no-cache --tag  java_lide ./java/
-#docker build  --no-cache --tag  php_lide ./php/
+# Ci-dessous la liste des images à build
+liste=( py cpp java )
+
+
+# éxécution en parrallele
+for img in ${liste[*]}
+do
+	docker build --no-cache --tag ${img}_lide ./${img}/ &
+done
+wait

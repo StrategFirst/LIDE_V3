cfe79439e9c965f2b1ff48b80cbaa056c3cfcc77
StrategFirst <mathieu.toulon@free.fr>
2022-01-09 19:32:52 +0100

    Changement mineur d'indentation

--- lide-back/controllers/export.controller.js
+++ lide-back/controllers/export.controller.js
@@ -3,35 +3,35 @@ const { execSync, exec } = require('child_process');
 const FileService = require('../services/db/file.service');
 
 exports.getExport = async (req, res) => {
-    const username = req.params.username;
+		const username = req.params.username;
 
-    try {
-    let result = await UserService.getProjects(username)
-        .catch((err) => {
-            res.status(400).json({
-                error: err.message,
-            });
-        });
-    
-    for (const projet of result){
-        for (const projetFile of projet.files){
-            //pas de filecontent dans getProjects alors on recharge les fichiers en entiers
-            const projectname = projet.projectname;
+		try {
+		let result = await UserService.getProjects(username)
+				.catch((err) => {
+						res.status(400).json({
+								error: err.message,
+						});
+				});
+		
+		for (const projet of result){
+				for (const projetFile of projet.files){
+						//pas de filecontent dans getProjects alors on recharge les fichiers en entiers
+						const projectname = projet.projectname;
 
-            // récupération du fichier
-            const file = await FileService.get(username, projetFile._id).catch((error) => { res.status(200).json(error); });
-            const filename = file.filename;
-            const extension = file.extension.replace('.', '');
-            const fileContent = file.content;
-            // Recréation
-            execSync('mkdir -p /lide-export/' + username + '/' + projectname);
+						// récupération du fichier
+						const file = await FileService.get(username, projetFile._id).catch((error) => { res.status(200).json(error); });
+						const filename = file.filename;
+						const extension = file.extension.replace('.', '');
+						const fileContent = file.content;
+						// Recréation
+						execSync('mkdir -p /lide-export/' + username + '/' + projectname);
 
-            let filePath = '/lide-export/' + username + '/' + projectname + '/' + filename + '.' + extension;
+						let filePath = '/lide-export/' + username + '/' + projectname + '/' + filename + '.' + extension;
 
-            // Remplir le contenu du fichier
-            execSync('echo \'' + fileContent + '\' > ' + filePath);
-        }
-    }
+						// Remplir le contenu du fichier
+						execSync('echo \'' + fileContent + '\' > ' + filePath);
+				}
+		}
 
 execSync('zip -r /lide-export/'+username + '/' + username +'.zip /lide-export/' + username + '/');
 
@@ -43,7 +43,7 @@ exec('sleep 3 && rm -Rf /lide-export/'+username);
 
 
 }catch (error) {
-    console.error("probleme sur le zip' :"+error);
-    res.status(400);    
-  }
+		console.error("probleme sur le zip' :"+error);
+		res.status(400);		
+	}
 };
--- lide-back/controllers/mail.controller.js
+++ lide-back/controllers/mail.controller.js
@@ -1,39 +1,39 @@
 const nodemailer = require('nodemailer');
 
 async function sendNotificationEmail(emailFrom, object, content){
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        // Il faut indiquer un Email et un mot de passe, tous les messagews du formulaire de contact seront envoyé à cet Email
-        user: '',
-        pass: ''
-      }
-    });
-      
-    const mailOptions = {
-      from: emailFrom,
-      to: 'barro.juliette@gmail.com',
-      subject: object,
-      text: content
-    };
+		let transporter = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				// Il faut indiquer un Email et un mot de passe, tous les messagews du formulaire de contact seront envoyé à cet Email
+				user: '',
+				pass: ''
+			}
+		});
+			
+		const mailOptions = {
+			from: emailFrom,
+			to: 'barro.juliette@gmail.com',
+			subject: object,
+			text: content
+		};
 
-    transporter.sendMail(mailOptions);
+		transporter.sendMail(mailOptions);
 }
 
 exports.post = (req, res) => {
-    const emailFrom = req.body.email;
-    const object = req.body.object;
-    const content = req.body.content;
+		const emailFrom = req.body.email;
+		const object = req.body.object;
+		const content = req.body.content;
 
-    let fromAdress = "LIDE visitor <" + emailFrom + ">";
-    
-    try{
-      sendNotificationEmail(fromAdress, object, content);
-      res.status(200).json({response: "Success"});
-    } catch(err){
-      res.status(500).json({response: "Fail: "+err.message});
-    }
+		let fromAdress = "LIDE visitor <" + emailFrom + ">";
+		
+		try{
+			sendNotificationEmail(fromAdress, object, content);
+			res.status(200).json({response: "Success"});
+		} catch(err){
+			res.status(500).json({response: "Fail: "+err.message});
+		}
 };
 

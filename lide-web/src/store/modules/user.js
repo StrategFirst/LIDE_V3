d3a7a41a4ff3cc13f885c4d32a2a450c6aa06087
StrategFirst <mathieu.toulon@free.fr>
2022-01-09 19:40:11 +0100

    Security and clean up Password should not be save in the localstorage or anywere in the client

--- lide-web/src/store/modules/user.js
+++ lide-web/src/store/modules/user.js
@@ -2,7 +2,6 @@ import service from "../../services/user-service";
 
 const state = () => ({
     username: "",
-    password: "",
     users: []
 });
 
@@ -10,9 +9,6 @@ const getters = {
     username (state) {
         return state.username;
     },
-    password (state) {
-	return state.password;
-    }
 }
 
 const actions = {
@@ -20,37 +16,27 @@ const actions = {
     setUsername({commit}, username){
         commit('SET_USERNAME', username)
     },
-    setPassword({commit}, password){
-        commit('SET_PASSWORD', password)
-    },
     async createUser(context) {
         let response = await service.createUser(context.getters.username, context.getters.password);
 	if( response.status == 200 ) {
             localStorage.username = context.getters.username;
             return response;
-	} else {
+	} else
             throw response;
-	}
     },
+
     async fetchUsers({ commit }) {
         await service.getAll()
-            .then(res => {
-                res.json().then(value => {
-                    commit("SET_USERS", value);
-                });
-            })
+            .then( res => res.json() )
+            .then( value => commit("SET_USERS", value) )
     },
+
 }
 
 const mutations = {
     SET_USERNAME(state, username){
         state.username = username;
     },
-    SET_PASSWORD(state, password){
-        state.password = password;
-        console.log( 'password set');
-    },
-
     SET_USERS(state, users){
         state.users = users;
     }

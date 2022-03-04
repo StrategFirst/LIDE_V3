56e2572f11e1327f176a6a46615cea2c8b5b1b2d
Mathieu <mathieu.toulon@free.fr>
2022-03-04 11:29:27 +0100

    another try

--- lide-web/src/components/utils/Notification.vue
+++ lide-web/src/components/utils/Notification.vue
@@ -11,13 +11,17 @@
 <script>
 import NotificationStore from '@/store/modules/notification.js';
 
-this.$store.subscribe((mutation,state) => {
-	if(mutation.type=='AJOUT_NOTIF') {
-		console.log('AJOUT_NOTIF',state);
-	}
-});
 
 export default {
 	props: ['name','localPath'],
+	methods: {
+		listen:function() {	
+			this.$store.subscribe((mutation,state) => {
+				if(mutation.type=='AJOUT_NOTIF') {
+					console.log('AJOUT_NOTIF',state);
+				}
+			});
+		}
+	}
 };
 </script>
